42bf4545ceb4c72d601da88a50bed1a3fd2c2e7b
Aakash Jangid <51054507+aakashjangidme@users.noreply.github.com>
2020-05-11 20:38:32 +0530

    added stateful widget, and onpressed method inside a function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,20 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// building a stateful widget
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      // to update the state of build when we click on the button
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
+            //Expanded takes the maximum available space in row/column/flex
+            child: FlatButton(
+              //puts an image
+              child: Image.asset(
+                  'images/dice$leftDiceNumber.png'), //new way to put an image as class
+              onPressed: () {
+                //onPressed action
+                changeDiceFace();
+              },
             ),
           ),
           Expanded(
-            child: Image.asset('images/dice2.png'),
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
           ),
         ],
       ),
